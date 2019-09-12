c09a625421e00c70cb15c6269731740310ee68c5
Stas <stanislavalex@mail.ru>
2019-09-12 23:54:29 +0300

    Add files via upload

--- Finder_of_winners_in_instagram_competition.py
+++ Finder_of_winners_in_instagram_competition.py
@@ -8,18 +8,16 @@ import sys
 def createparser():
     parser = argparse.ArgumentParser()
     parser.add_argument('url')
+    parser.add_argument('name_of_page')
     return parser
 
 def get_comments(all_comments):
     comments = []
-    correct_comment = re.findall(r"@([a-zA-Z0-9]{1,15})", str(comment_text["text"]))
     for comment_text in all_comments:
+        correct_comment = re.findall(r"@([a-zA-Z0-9]{1,15})", str(comment_text["text"]))
         if correct_comment !=[]:
             comments.append(correct_comment)
     return comments
-
-def is_user_exist(user_id):
-    return user_id == None
 
 def get_likers_users(existing_users):
     for existing_user_id in existing_users:
@@ -35,12 +33,11 @@ def get_likers_users(existing_users):
                 pass      
     return existing_users 
 
-def get_followers(exist_and_likers_user):   
-    for existing_user_id in exist_and_likers_user:
-        followers_user = bot.get_user_followers("beautybar.rus")	
-        if followers_user[0] != existing_user_id[0]:
-            exist_and_likers_user.remove(existing_user_id)
-    return exist_and_likers_user        
+def get_followers(exist_and_likers_user):  
+    followers_user = bot.get_user_followers(cmd_argument.name_of_page)
+    exist_and_likers_usersid = (user_id[0] for user_id in exist_and_likers_user)    
+    final_list_of_usersid = (set(followers_user) & set(exist_and_likers_usersid))
+    return final_list_of_usersid        
 
 if __name__ == '__main__': 
 
@@ -58,19 +55,19 @@ if __name__ == '__main__':
 
     comments = get_comments(all_comments)
 
+    existing_users = []
     for user_name in comments:
         try:
             applicant_user_name, friend_user_name = user_name
         except ValueError:
             pass    
         user_id = bot.get_user_id_from_username(applicant_user_name)
-        existing_users = []
-        if is_user_exist(user_id):
+        if user_id != None:
             try:	
                 friend_user_id = bot.get_user_id_from_username(friend_user_name)
             except IndexError: 
                 pass   	
-            if is_user_exist(friend_user_id):
+            if friend_user_id != None:
                 username_and_id = (user_id, applicant_user_name)
                 try: 		
                     existing_users.append(username_and_id)
@@ -78,8 +75,8 @@ if __name__ == '__main__':
                     pass  
 
     exist_and_likers_user = get_likers_users(existing_users)
-    foll_exist_likes_user = get_followers(exist_and_likers_user)
-
-    for number, name_of_user in enumerate(foll_exist_likes_user, 1):
-        final_list = set(name_of_user)	
-        print("{0}. {1}".format(number, final_list))
+    final_list_of_usersid = get_followers(exist_and_likers_user)
+    
+    for number, userid in enumerate(final_list_of_usersid):
+        name_of_user = bot.get_username_from_user_id(userid)	
+        print("{0}. {1}".format(number, name_of_user))
