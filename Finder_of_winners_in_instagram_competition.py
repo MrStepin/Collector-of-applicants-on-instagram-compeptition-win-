aea2dd53d616214617eb06e50fa8070af2ecc8f0
Stas <stanislavalex@mail.ru>
2019-12-15 14:38:36 +0300

    Add files via upload

--- Finder_of_winners_in_instagram_competition.py
+++ Finder_of_winners_in_instagram_competition.py
@@ -11,13 +11,13 @@ def createparser():
     parser.add_argument('name_of_page')
     return parser
 
-def get_comments(all_comments):
-    comments = []
+def get_comment_author(all_comments):
+    comment_author = []
     for comment_text in all_comments:
         correct_comment = re.findall(r"@([a-zA-Z0-9]{1,15})", str(comment_text["text"]))
-        if correct_comment !=[]:
-            comments.append(correct_comment)
-    return comments
+        if correct_comment:
+            comment_author.append(correct_comment)
+    return comment_author
 
 def get_likers_users(existing_users):
     for existing_user_id in existing_users:
@@ -34,40 +34,40 @@ def get_likers_users(existing_users):
     return existing_users 
 
 def get_followers(exist_and_likers_user):  
-    followers_user = bot.get_user_followers(cmd_argument.name_of_page)
-    exist_and_likers_usersid = (user_id[0] for user_id in exist_and_likers_user)    
-    final_list_of_usersid = (set(followers_user) & set(exist_and_likers_usersid))
-    return final_list_of_usersid        
+    followers_user = set(bot.get_user_followers(cmd_arguments.name_of_page))
+    exist_and_likers_usersid = set(user_id[0] for user_id in exist_and_likers_user)    
+    follower_ids = (followers_user & exist_and_likers_usersid)
+    return follower_ids        
 
 if __name__ == '__main__': 
 
     load_dotenv()
-    login = os.getenv("login")
-    password = os.getenv("password")	
+    login = os.getenv("USERNAME")
+    password = os.getenv("PASSWORD")	
     bot = Bot()
     bot.login(username = login, password = password)
 
     entered_links = createparser()
-    cmd_argument = entered_links.parse_args()
+    cmd_arguments = entered_links.parse_args()
 
-    post_id = bot.get_media_id_from_link(cmd_argument.url)
+    post_id = bot.get_media_id_from_link(cmd_arguments.url)
     all_comments = bot.get_media_comments_all(post_id)
 
-    comments = get_comments(all_comments)
+    comment_author = get_comment_author(all_comments)
 
     existing_users = []
-    for user_name in comments:
+    for user_name in comment_author:
         try:
             applicant_user_name, friend_user_name = user_name
         except ValueError:
             pass    
         user_id = bot.get_user_id_from_username(applicant_user_name)
-        if user_id != None:
+        if user_id:
             try:	
                 friend_user_id = bot.get_user_id_from_username(friend_user_name)
             except IndexError: 
                 pass   	
-            if friend_user_id != None:
+            if friend_user_id:
                 username_and_id = (user_id, applicant_user_name)
                 try: 		
                     existing_users.append(username_and_id)
@@ -75,8 +75,8 @@ if __name__ == '__main__':
                     pass  
 
     exist_and_likers_user = get_likers_users(existing_users)
-    final_list_of_usersid = get_followers(exist_and_likers_user)
+    follower_ids = get_followers(exist_and_likers_user)
     
-    for number, userid in enumerate(final_list_of_usersid):
+    for number, userid in enumerate(follower_ids):
         name_of_user = bot.get_username_from_user_id(userid)	
         print("{0}. {1}".format(number, name_of_user))
